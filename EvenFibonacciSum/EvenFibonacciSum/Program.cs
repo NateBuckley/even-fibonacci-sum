35174d60ea8e6ef47b2e47b3385e09018a0a5a70
NateBuckley <albinoblacksheep0@yahoo.com>
2016-01-14 17:27:02 -0600

    added project

---
+++ EvenFibonacciSum/EvenFibonacciSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenFibonacciSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long fib1 = 2;
+            long fib2 = 0;
+            long result = 2;
+            long sum = 0;
+
+            while (result <= 4000000)
+            {
+                sum += result;
+
+                result = 4 * fib1 + fib2;
+                fib2 = fib1;
+                fib1 = result;
+            }
+
+            Console.WriteLine("The sum of the even Fibonacci sequence numbers less than 4 million: {0}", sum);
+
+            Console.Read();
+        }
+    }
+}
